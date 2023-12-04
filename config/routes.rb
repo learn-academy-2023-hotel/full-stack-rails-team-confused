7fda4ba23cd2837c3e0cded71c2d9ee344daddab
Joseph Kong <josephk1018@gmail.com>
2023-12-04 15:31:48 -0800

    add create method and going back to land page after submitting

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/blogs' => 'blog#index', as: 'blogs'
   get '/blogs/new' => 'blog#new', as: 'new_blog'
   get '/blogs/:id' => 'blog#show', as: 'blog'
+  post '/blogs' => 'blog#create'
 
   # Defines the root path route ("/")
   # root "articles#index"
