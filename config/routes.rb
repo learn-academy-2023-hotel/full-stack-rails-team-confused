71a50a186595c01d703d0d7e1a712c664b526503
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-12-04 15:14:45 -0800

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/blogs' => 'blog#index', as: 'blogs'
+  get '/blogs/new' => 'blog#new', as: 'new_blog'
   get '/blogs/:id' => 'blog#show', as: 'blog'
+
   # Defines the root path route ("/")
   # root "articles#index"
   root "blog#index"
